728c0d6f98baaae24eb2fd8a3b0a8f63d886184f
d-s-t-e-n-z-e-l <desiree.stenzel@gmx.de>
2023-10-20 23:09:34 +0200

    template & resp.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function viewRecommendations(){
     document.getElementById('recommentProfils').classList.remove('d-none')
 }
 
+function setAbo(i){
+    let buttonID = 'abo'+i;
+    document.getElementById(buttonID).innerHTML = 'Abonniert';
+    
+}
+
 let posts = [
     {
         "profilbild": "profilmom.jpg" ,
@@ -10,7 +16,6 @@ let posts = [
         "Stunden": 1,
         "image": "codemom.jpg",
         "header": "Voll bei der Sache",
-        "description": "Wenn man plötzlich an nichts anderes mehr denken kann...",
         "comments": ['SAAAME, aber mit Schoki', 'Ist das Styropor?', 'Hat deine kleine denn einen Bug gefunden?? :)'],
         "users": ['chantalG', 'anni', 'SvenB85'],
     },
@@ -21,8 +26,7 @@ let posts = [
         "Stunden": 3,
         "image": "katerchen.jpg",
         "header": "Ausgesperrt?",
-        "description": "Bin ich aus oder ist Frauchen eingesperrt??????",
-        "comments": ['ich würde eher fragen ob es es drinnen warm ist!', 'du darfst tagsüber raus?', 'du ausgesperrt! ganz klar! '],
+        "comments": ['ich würde fragen ob es es drinnen warm ist!', 'du darfst tagsüber raus?', 'du ausgesperrt! ganz klar! '],
         "users": ['blackPanther', 'catInside', 'Abra Catdabra'],
     },
     {
@@ -32,7 +36,6 @@ let posts = [
         "Stunden": 15,
         "image": "matticold.jpg",
         "header": "Its cold as ice!",
-        "description": "Ich war heute das erste mal in der Kältekammer in Postdam und habe 3 Minuten bei -110 Grad Celsius voll abgedanct! Sehr erfrischend und gesund",
         "comments": ['Ich geh auch öfter hin, es ist echt toll!', 'Freiwillig?', 'Wie klein ääh ich mein kalt war es denn :D'],
         "users": ['codingMom', 'The Miezer', 'SBenni'],
     }
